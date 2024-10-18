460ef3ea75f07989f6d63da6093d1a33a37dddf2
Jean Sebastian French <sebas03446@gmail.com>
2024-10-18 15:40:47 +0200

    feat: add initial configuration of controler

---
+++ src/application/contoler/CalculatorControler.java
@@ -0,0 +1,9 @@
+package application.contoler;
+
+public class CalculatorControler {
+    public CalculatorControler() {}
+
+    public void change(String accu){
+
+    }
+}
---
+++ src/application/contoler/CalculatorControlerInterface.java
@@ -0,0 +1,6 @@
+package application.contoler;
+
+public interface CalculatorControlerInterface {
+    public void change(String accu);
+    public void change(List<Double> stackData);
+}
